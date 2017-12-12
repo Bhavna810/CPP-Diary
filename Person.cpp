d11dec491ba4b2e334d94e1bddabe5c4fba01bcf
Bhavna Gupta <bhavnagupta810@gmail.com>
2017-12-12 01:32:21 -0600

    WIP class

--- Person.cpp
+++ Person.cpp
@@ -36,52 +36,56 @@ Person::Person()
 
 void Person::set_firstname(string fname)
 {
-	
+	m_firstname = fname;
 }
 
 string Person::get_firstname()
 {
-	
+	return m_firstname;
 }
 
 void Person::set_lastname(string lname)
 {
-	
+	m_lastname = lname;
 }
 
-string get_lastname()
+string Person::get_lastname()
 {
-	
+	return m_lastname;
 }
 
 void Person::set_gender(string gender)
 {
-	
+	m_gender = gender;
 }
 
 string Person::get_gender()
 {
-	
+	return m_gender;
 }
 
 void Person::set_age(int age)
 {
-	
+	if ( m_age < 0 || m_age > 120){
+		cout << "Invalid age" << endl;
+	} else {
+		m_age = age;
+	}
 }
 
 int Person::get_age()
 {
-	
+	return m_age;
 }
 
 void Person::set_account(double acc_bal)
 {
-	
+	m_account = acc_bal;
 }
 
 double Person::get_account()
 {
-	
+	return m_account;
 }
 
 int main()
