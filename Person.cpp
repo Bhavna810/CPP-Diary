f7cdcadf049ea8925e5f9268f6841a9aca5990ca
Bhavna Gupta <bhavnagupta810@gmail.com>
2017-12-12 01:26:33 -0600

    Class

--- Person.cpp
+++ Person.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class Person
 {
 public:
+	Person();
 	void set_firstname(string fname);
 	string get_firstname();
 	void set_lastname(string lname);
@@ -23,6 +24,65 @@ private:
 	int m_age;
 	double m_account;
 };
+
+Person::Person()
+{
+	m_firstname = "";
+	m_lastname = "";
+	m_gender = "";
+	m_age = 0;
+	m_account = 0.0;
+}
+
+void Person::set_firstname(string fname)
+{
+	
+}
+
+string Person::get_firstname()
+{
+	
+}
+
+void Person::set_lastname(string lname)
+{
+	
+}
+
+string get_lastname()
+{
+	
+}
+
+void Person::set_gender(string gender)
+{
+	
+}
+
+string Person::get_gender()
+{
+	
+}
+
+void Person::set_age(int age)
+{
+	
+}
+
+int Person::get_age()
+{
+	
+}
+
+void Person::set_account(double acc_bal)
+{
+	
+}
+
+double Person::get_account()
+{
+	
+}
 
 int main()
 {
