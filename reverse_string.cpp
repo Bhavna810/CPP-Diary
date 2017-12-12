c0d487682bb1b0f2d12b3cdb76b470257ab35d94
Bhavna Gupta <bhavnagupta810@gmail.com>
2017-12-12 00:34:35 -0600

    Reverse string

---
+++ reverse_string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string reverse(string word);
+
+int main()
+{
+	string word = " ";
+	cout << "Give a word that you would to reverse: " << endl;
+	cin >> word;
+	string resultant_string = reverse(word);
+}
+
+string reverse(string reverse)
+{
+	//for( int i = word.length() - 1; i < word.length(); i--)word[i]
+	
+}
