94dd91a9bd5402a524c40a1347409425066bf59a
Bhavna Gupta <bhavnagupta810@gmail.com>
2018-01-05 07:46:53 -0600

    Working on fixing outer loop

--- selection_sort/selection_sort.cpp
+++ selection_sort/selection_sort.cpp
@@ -16,24 +16,33 @@ int main()
 	
 	sel_sort(arr, size);
 	
+	std::cout << "Sorted array is: " << std::endl;
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+	
 	return 0;
 }
 
 void sel_sort(int arr[], int size)
 {
 	
-	int min_num = arr[0];
+	int min_num = arr[0], temp = 0;
 	
-//	for (int i = 0; i < size; i++)
-//	{	
+	
+	for (int i = 0; i < size; i++)
+	{	
 		for (int j = 0; j < size; j++) 
 		{
 			if ( arr[j] < min_num )
 			{
 				min_num = arr[j];
 			}
-			std::cout << min_num << std::endl;
+			arr[i] = temp;
+			arr[i] = min_num;
+			temp = min_num;
 		}
-//	}	
-	std::cout << min_num << std::endl;
+	}	
 }
