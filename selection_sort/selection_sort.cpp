272b8d4844491bc0f2d8c840aef9bf3b256dff80
Bhavna Gupta <bhavnagupta810@gmail.com>
2018-01-05 07:10:06 -0600

    Finding minimum value

--- selection_sort/selection_sort.cpp
+++ selection_sort/selection_sort.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 
-void sel_sort(int arr[], size_t size);
+void sel_sort(int arr[], int size);
 
 int main()
 {	
-	int arr[ ] = {5, 9, 7, 1, 2, 4, 8, 3, 0, 6};
+	int size = 10;
+	int arr[] = {5, 9, 7, 1, 2, 4, 8, 3, 0, 6};
 	
 	std::cout << "Initial unsorted array is: " << std::endl;
-	for(int i = 0; i < 10; i++){
+	for (int i = 0; i < size; i++)
+	{
 		std::cout << arr[i] << " ";
 	}
 	std::cout << std::endl;
 	
+	sel_sort(arr, size);
+	
 	return 0;
+}
+
+void sel_sort(int arr[], int size)
+{
+	
+	int min_num = arr[0];
+	
+//	for (int i = 0; i < size; i++)
+//	{	
+		for (int j = 0; j < size; j++) 
+		{
+			if ( arr[j] < min_num )
+			{
+				min_num = arr[j];
+			}
+			std::cout << min_num << std::endl;
+		}
+//	}	
+	std::cout << min_num << std::endl;
 }
