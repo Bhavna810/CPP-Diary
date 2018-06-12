b2f41da65e8e0c45b6977d4573c037a55bf4c8a0
Bhavna810 <bhavnagupta810@gmail.com>
2018-06-12 12:09:57 -0500

    I changed the return type and some other stuff

--- recursion_assignment/recursion_prog.cpp
+++ recursion_assignment/recursion_prog.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 
-void recursive_func (int num)
+int recursive_func (int num)
 {
+	int val = 0;
 	 // If the value is less than or equal to 0, go back to the main()
     if (num < 1)
-        return;
+        return val;
     else
-        std::cout << "Value is " << num << std::endl;
-		
-		int val = num;
-		
-		if (val > 1) {
-			int fac = num*--val;
-			std::cout << fac << std::endl;
-		}    
+		std::cout << "Value is " << num << std::endl;
+		int a = --num;
+		int fac = num*a;
+		val = recursive_func(a);
+		std::cout << val << std::endl;
+		return fac;
+	//int val = num;
+	//if (val > 1){
+	//	int fac = num*--val;
+	//	std::cout << fac << std::endl;
+	//}    
     // Keep recursing but decrease the value
-    recursive_func(--num);
+    //recursive_func(--num);
 }
 
 int main ()
 {
     int n = 5;
-    recursive_func(n);
-
+	int fac = 0;
+    fac = recursive_func(n);
+	
+	std::cout << "Factorial is " << fac << std::endl;
+	
     return 0;
 }
